872b103c368099cb64735b85e05135ee6dae60c4
Richard Wade <rik@rikwade.com>
2018-09-13 23:18:52 +0800

    initial commit

---
+++ addOne.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+'''
+[1,3,4]->[1,3,5]
+[1,9,9]->[2,0,0]
+[9,9,9]->[1,0,0,0]
+'''
+def addOne(inArray):
+    # create a copy of the inArray
+    tmpArray = inArray.copy()
+    carry = 1
+
+    for i in range(len(tmpArray),0,-1):
+        pos = i-1
+        tmpArray[pos] = tmpArray[pos]+carry
+        if (tmpArray[pos] > 9):
+            tmpArray[pos] = 0
+            carry = 1
+            if (pos == 0):
+                tmpArray.insert(0,1)
+        else:
+            carry = 0
+    return(tmpArray)
+
+#Main
+newArray = addOne([9,9,9,9,9,9])
+
+print("newArray:", newArray)
